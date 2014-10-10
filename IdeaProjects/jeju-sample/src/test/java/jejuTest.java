49fdf268e21e8fb8b06d6dff69bc72eed1edae26
AppleMint <llyyyy7@gmail.com>
2014-10-10 17:17:53 +0900

    14.10.10 poam.xml add

---
+++ IdeaProjects/jeju-sample/src/test/java/jejuTest.java
@@ -0,0 +1,15 @@
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+/**
+ * Created by AppleMint on 2014. 10. 10..
+ */
+public class jejuTest {
+    @org.junit.Test
+    public void testAdd() throws Exception {
+        assertThat(1 + 1, is(2));
+
+
+    }
+
+}
