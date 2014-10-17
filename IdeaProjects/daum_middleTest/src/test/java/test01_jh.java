3b1d3723eba54c95e4a4e0e0144131d1b7cdf6c2
AppleMint <llyyyy7@gmail.com>
2014-10-17 22:46:42 +0900

    2012104139_양지현_First Test Commit

---
+++ IdeaProjects/daum_middleTest/src/test/java/test01_jh.java
@@ -0,0 +1,18 @@
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Created by AppleMint on 14. 10. 17..
+ */
+public class test01_jh {
+
+    public void addTest01(){
+        int a = 7;
+        int b = 8;
+
+        assertThat(a+b,is(15));
+    }
+}
+
+
+
