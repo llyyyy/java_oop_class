8254fdcb4e908a7c718b6379f75db8a5f0f5ad2b
AppleMint <llyyyy7@gmail.com>
2014-10-17 23:02:32 +0900

    2012104139_양지현_Second Test Commit

---
+++ IdeaProjects/daum_middleTest/src/test/java/test02_jh.java
@@ -0,0 +1,15 @@
+import static org.hamcrest.CoreMatchers.is;
+        import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Created by AppleMint on 14. 10. 17..
+ */
+public class test02_jh {
+
+    public void multest02() {
+        int a = 3;
+        int b = 3;
+
+        assertThat(a*b, is(9));
+    }
+}
